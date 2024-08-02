c1502b78db43ec065df7640d6e81207846d3c485
140bpmdubstep <laineprikol@gmail.com>
2024-08-02 21:57:02 +0300

    Mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FilterComponent} from "./filter/filter.component";
+import {TradingTableComponent} from "./trading-table/trading-table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FilterComponent],
+  imports: [CommonModule, FilterComponent, TradingTableComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
